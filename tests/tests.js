b5256099a248cc5e0694005af9d8421c9baff53a
dhershman1 <dustinh17@gmail.com>
2016-11-09 17:10:06 -0500

    made pathit return a promise instead of using a callback

--- tests/tests.js
+++ tests/tests.js
@@ -6,51 +6,56 @@ const fs = require('fs');
 
 describe('Creating a file based on string', function() {
 	it('should create a simple path', function(done) {
-		pathit(['./tests/testpath'], function() {
-			fs.readdir('./tests/testpath', (err, files) => {
-				if (err) done(err);
-				expect(files).to.be.ok;
-				done();
+		pathit(['./tests/testpath'])
+			.then(function() {
+				fs.readdir('./tests/testpath', (err, files) => {
+					if (err) done(err);
+					expect(files).to.be.ok;
+					done();
+				});
 			});
-		});
 	});
 	it('should create a subfolder 1', function(done) {
-		pathit(['./tests/testpath/new-path-1'], function() {
-			fs.readdir('./tests/testpath', (err, files) => {
-				if (err) done(err);
-				expect(files.length).to.equal(1);
-				done();
+		pathit(['./tests/testpath/new-path-1'])
+			.then(function() {
+				fs.readdir('./tests/testpath', (err, files) => {
+					if (err) done(err);
+					expect(files.length).to.equal(1);
+					done();
+				});
 			});
-		});
 	});
 	it('should create a 2nd subfolder', function(done) {
-		pathit(['./tests/testpath/path_2'], function() {
-			fs.readdir('./tests/testpath', (err, files) => {
-				if (err) done(err);
-				expect(files.length).to.equal(2);
-				done();
+		pathit(['./tests/testpath/path_2'])
+			.then(function() {
+				fs.readdir('./tests/testpath', (err, files) => {
+					if (err) done(err);
+					expect(files.length).to.equal(2);
+					done();
+				});
 			});
-		});
 	});
 	it('should create a whole branch', function(done) {
-		pathit(['./tests/testpath/path_3/sub_path'], function() {
-			fs.readdir('./tests/testpath/path_3', (err, files) => {
-				if (err) done(err);
-				expect(files.length).to.equal(1);
-				done();
+		pathit(['./tests/testpath/path_3/sub_path'])
+			.then(function() {
+				fs.readdir('./tests/testpath/path_3', (err, files) => {
+					if (err) done(err);
+					expect(files.length).to.equal(1);
+					done();
+				});
 			});
-		});
 	});
 });
 
 describe('Handling multiple paths in an array', function() {
 	it('Should create multiple paths', function(done) {
-		pathit(['./tests/multi_path_test', './tests/multi_path_test/path_1', './tests/multi_path_test/path_2', './tests/multi_path_test/path_3'], function() {
-			fs.readdir('./tests/testpath', (err, files) => {
-				if (err) done(err);
-				expect(files).to.be.ok;
-				done();
+		pathit(['./tests/multi_path_test', './tests/multi_path_test/path_1', './tests/multi_path_test/path_2', './tests/multi_path_test/path_3'])
+			.then(function() {
+				fs.readdir('./tests/testpath', (err, files) => {
+					if (err) done(err);
+					expect(files).to.be.ok;
+					done();
+				});
 			});
-		});
 	});
 });
